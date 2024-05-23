897f095478e972891e7fc65effd222f730732c9c
jazeps-bumbiers <jaazeps.bumbiers@gmail.com>
2024-05-23 18:11:42 +0300

    Infinite scroll done

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app-bar :elevation="2">
+        <v-app-bar-title>AndeleMandele test task - {{ pageTitle }}</v-app-bar-title>
+    </v-app-bar>
+</template>
+
+<script setup>
+    import { computed } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+    
+    const pageTitle = computed(() => {
+        switch (route.name) {
+            case 'home':
+                return `Home`;
+        }
+    });
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-bottom-navigation
+        color="primary"
+        horizontal
+    >
+        <v-btn value="home" @click="navigateToRoute('/')">
+            <v-icon icon="mdi-home-account" size="x-large"></v-icon>
+
+            <span class="mt-2">
+                Home
+            </span>
+        </v-btn>
+    </v-bottom-navigation>
+</template>
+
+<script setup>
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+
+    const navigateToRoute = (path) => {
+        router.push(path);
+    };
+</script>
